9ef1519257c134fa1215fcfb0c1e52f6747fe359
47 <bendestiny1289@gmail.com>
2019-07-13 05:11:58 +0300

    Take user input forms from html and save it as variables in javascript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+var days=["sunday","tuesday","wenesday"]
 function test(){
     var gender=document.forms["akhan"]["gender"].value
-    
-    
+    var century=document.forms["akhan"]["century"].value
+    var year=document.forms["akhan"]["year"].value
+    var month=document.forms["akhan"]["year"].value
+    var date=document.forms["akhan"]["year"].value
 }
