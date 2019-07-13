d9a3df18f8447b16b9d939b4f76b2db823c73097
47 <bendestiny1289@gmail.com>
2019-07-13 06:43:53 +0300

    add header class and a navigation links at the home page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var days=["sunday","tuesday","wenesday"]
+var days=["sunday","monday","tuesday","wenesday","thursady","friday","saturday",]
+var namesMale=[]
+var namesFemale=[]
 function test(){
     var gender=document.forms["akhan"]["gender"].value
     var century=document.forms["akhan"]["century"].value
     var year=document.forms["akhan"]["year"].value
-    var month=document.forms["akhan"]["year"].value
-    var date=document.forms["akhan"]["year"].value
+    var month=document.forms["akhan"]["month"].value
+    var Date=document.forms["akhan"]["date"].value
 }
