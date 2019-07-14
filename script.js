7743e22c8310febb00a1a599d1b5f3f5056023f6
47 <bendestiny1289@gmail.com>
2019-07-14 17:25:56 +0300

    add paragraph to home page

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var days=["sunday","monday","tuesday","wenesday","thursady","friday","saturday",
 var namesMale=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var namesFemale=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 function test(){
-    var gender=document.forms["akhan"]["gender"].value
-    var century=document.forms["akhan"]["century"].value
-    var year=document.forms["akhan"]["year"].value
-    var month=document.forms["akhan"]["month"].value
-    var Date=document.forms["akhan"]["date"].value
+    var gender=document.getElementById
+    var century=document.getElementById
+    var year=document.getElementById
+    var month=document.getElementById
+    var date=document.getElementById
+
+
+
+    var specificDay = ( ( (century/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7
+alert(test)
 }
+
