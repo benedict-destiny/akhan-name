0f5c07d58b09f1f5121035e23f126a5102218699
47 <bendestiny1289@gmail.com>
2019-07-15 08:30:01 +0300

    add user input form

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
 
+function giveName() {
+  var name = document.getElementById("yourNames").value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
 
 function selectGender() {
   var gender = document.getElementById("gender").value;
